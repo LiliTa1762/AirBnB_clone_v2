da1282e22f5ef6776e670e15014cca3971fbf680
Lilita1762 <lilibeth.tabares@gmail.com>
2021-01-20 21:13:04 +0000

    List of states. Task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Start Flask web app using templates"""
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """To display States"""
+    n = storage.all(States).values()
+    return render_template('7-states_list.html', n=n)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
